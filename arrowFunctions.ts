9aa42f03d61c77d65ba4e10d3c266ae7fb1d1f0e
purut18 <puruthakkar180901@gmail.com>
2019-09-05 23:35:59 +0530

    Added Arrow Function Typescript

---
+++ arrowFunctions.ts
@@ -0,0 +1,16 @@
+// regular function
+let log = function (message) {
+    console.log(message);
+}
+
+// arrow function
+let doLog = (message) => {
+    console.log(message);
+};
+
+// arrow function for one line functions
+let deLog2 = (message) => console.log(message);
+
+// you can also ignore brackets around parameter
+// if only one parameter is there
+let deLog3 = message => console.log(message);
